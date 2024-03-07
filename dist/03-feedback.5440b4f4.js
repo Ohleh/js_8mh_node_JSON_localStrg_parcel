fc6dcf1bc858f7edbf3cf7c83fe9ad0891a2ef35
Ohleh <oleg.kolischuk@gmail.com>
2024-03-07 22:23:07 +0200

    add form localeStrg input

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -503,6 +503,41 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"eH52W":[function(require,module,exports) {
+const formRef = document.querySelector(".feedback-form");
+formRef.addEventListener("input", onInput);
+formRef.addEventListener("submit", onSubmit);
+const userInfo = "feedback-form-state";
+checkStorage();
+function onInput(e) {
+    e.preventDefault();
+    const el = e.target.form.elements;
+    const userData = {
+        email: el.email.value,
+        message: el.message.value
+    };
+    const setDataStorage = JSON.stringify(userData);
+    localStorage.setItem(userInfo, setDataStorage);
+//   if (e.target.form.elements[2].type === "submit") {
+//     localStorage.clear();
+//     formRef.elements.email.value = "";
+//     formRef.elements.message.value = "";
+//   }
+}
+function checkStorage() {
+    if (!localStorage.getItem(userInfo)) return console.log("localStorage is empty");
+    else {
+        const getDataStorage = localStorage.getItem(userInfo);
+        const dataStogrageUnparce = JSON.parse(getDataStorage);
+        formRef.elements.email.value = dataStogrageUnparce.email;
+        formRef.elements.message.value = dataStogrageUnparce.message;
+    }
+}
+function onSubmit(e) {
+    e.preventDefault();
+    localStorage.clear();
+    formRef.elements.email.value = "";
+    formRef.elements.message.value = "";
+}
 
 },{}]},["7Ih8d","eH52W"], "eH52W", "parcelRequired7c6")
 
